3bf6e90a7e0d456e583a74e813fda27ae3f66c24
Chris Kalla <ChristopherKalla@MacBook-Pro.local>
2018-04-15 22:29:00 -0700

    adding changes

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: 'XXSTctnDco2qZsGrrv5D3D3xQ',
+  consumer_secret: 'hW4yQClk9brAkvUOWjEYWV6uvwjtF2kn9YjTXKrUfbGoLKdpUl',
+  access_token_key: '985575518111055872-o9WeH586kmQNUAsilaM1d5sPyCQ9SEQ',
+  access_token_secret: '2KFtS1lFYNvYpzkF7MF5MdHulWGFSiu3e47iqndhOFH4o'
+};
+
+exports.spotify = {
+  id: '2e6381b250374cabb448fb7fde3b0d16',
+  secret: '084d841bb31242c4a96e4cdf6f5fbbf7'
+};
